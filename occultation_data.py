dd6eb9fd52f8f086cfd70edd6d19397ea3f8c6a5
Chris Mankovich <cmankovich@ucsc.edu>
2026-03-12 13:58:30 -0700

    improved Jupiter solution using Helled+2009 tabulated occultation radii

--- occultation_data.py
+++ occultation_data.py
@@ -7,18 +7,30 @@ uranus = {'voyager 2 ingress': (-2.9, 25555.3), 'voyager 2 egress': (-6.6, 25550
 # Neptune occultations, Lindal 1992
 neptune = {'voyager 2 egress': (-40.07, 24599.58)}
 
-# Jupiter occultations, Lindal et al. 1981, 100-mbar surface
-# fair warning! these occultation radii were pulled from the pixellated plot in Lindal et al. 1981's Figure 7.
-# I did my best but expect these are only accurate to within 5 km, similar to Lindal's estimated standard deviation of 4 km.
-# identifying which are ingress versus egress from Lindal is a squinty endeavor but they are given in Buccino et al. (2020 JGR:Planets) Figure 1
-jupiter = {
-    'pioneer 10 ingress':(60.94052, 67.92648e3),
-    'pioneer 10 egress':(28.19087, 70.39084e3),
-    'pioneer 11 egress':(19.70111, 70.93895e3),
-    'voyager 1 egress':(0.16012, 71.53487e3),
-    'voyager 1 ingress':(-10.17727, 71.37230e3),
-    'voyager 2 ingress':(-70.62033, 67.36508e3),
-}
+if False:
+    # Jupiter occultations, Lindal et al. 1981, 100-mbar surface
+    # fair warning! these occultation radii were pulled from the pixellated plot in Lindal et al. 1981's Figure 7.
+    # I did my best but expect these are only accurate to within 5 km, similar to Lindal's estimated standard deviation of 4 km.
+    # identifying which are ingress versus egress from Lindal is a squinty endeavor but they are given in Buccino et al. (2020 JGR:Planets) Figure 1
+    jupiter = {
+        'pioneer 10 ingress':(60.94052, 67.92648e3),
+        'pioneer 10 egress':(28.19087, 70.39084e3),
+        'pioneer 11 egress':(19.70111, 70.93895e3),
+        'voyager 1 egress':(0.16012, 71.53487e3),
+        'voyager 1 ingress':(-10.17727, 71.37230e3),
+        'voyager 2 ingress':(-70.62033, 67.36508e3),
+    }
+else:
+    # instead take values from Helled et al. 2009's Table 2. they also scraped from Lindal et al. 1981, with slightly different results.
+    # these are the same points that Galanti et al. 2023 (GRL) compared to.
+    jupiter = {
+        'pioneer 10 ingress':(60.3, 67933.93),
+        'pioneer 10 egress':(28., 70415.08),
+        'pioneer 11 egress':(19.8, 70943.95),
+        'voyager 1 egress':(0.07, 71538.61),
+        'voyager 1 ingress':(-10.1, 71378.73),
+        'voyager 2 ingress':(-71.8, 67293.64),
+    }
 
 # 100-mbar radii from Juno occultations
 import numpy as np
